608e7c05d112fe82bc53ef43bdd036e0dac06be1
Levi Ferreira <levi.mferreira@gmail.com>
2022-07-23 13:56:41 -0300

    Allow CORS only to frontend

--- src/main/java/com/levimartines/mylearningbackend/config/SecurityConfig.java
+++ src/main/java/com/levimartines/mylearningbackend/config/SecurityConfig.java
@@ -4,15 +4,17 @@ import com.levimartines.mylearningbackend.security.JWTAuthenticationFilter;
 import com.levimartines.mylearningbackend.security.JWTAuthorizationFilter;
 import com.levimartines.mylearningbackend.security.JWTUtil;
 
-import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,11 +23,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
     private final JWTUtil jwtUtil;
+    private final String frontendUrl;
 
     private static final String[] PUBLIC_MATCHERS_GET = {
         "/tasks"
@@ -35,15 +38,21 @@ public class SecurityConfig {
     };
 
     public SecurityConfig(
-        @Qualifier("customUserDetailsService") UserDetailsService userDetailsService,
-        JWTUtil jwtUtil) {
+        @Qualifier("customUserDetailsService") UserDetailsService userDetailsService, JWTUtil jwtUtil,
+        @Value("${service.frontend-host}") String frontendUrl) {
         this.userDetailsService = userDetailsService;
         this.jwtUtil = jwtUtil;
+        this.frontendUrl = frontendUrl;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
+        http.headers()
+            .xssProtection()
+            .and()
+            .contentSecurityPolicy("script-src 'self'");
+        http.cors(Customizer.withDefaults());
+        http.csrf().disable();
         http.authorizeHttpRequests(authz -> authz
             .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
             .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
@@ -63,8 +72,10 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(frontendUrl));
+        configuration.setAllowedMethods(List.of("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
