0c021b1ad6f0a40622a97c3d4d4b59423ae6af43
leviferreira <levi.mferreira@gmail.com>
2022-08-27 14:12:16 -0300

    Add cache on UserRepository.findByEmailIgnoreCase

--- src/main/java/com/levimartines/mylearningbackend/repositories/UserRepository.java
+++ src/main/java/com/levimartines/mylearningbackend/repositories/UserRepository.java
@@ -4,9 +4,11 @@ import com.levimartines.mylearningbackend.models.entities.User;
 
 import java.util.Optional;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Cacheable(value = "users", unless = "#result == null")
     Optional<User> findByEmailIgnoreCase(String email);
 }
